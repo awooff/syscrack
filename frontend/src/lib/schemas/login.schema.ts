73bba826655655c71cbabb95ead56e27cf93402c
kaelta <chxrry@ik.me>
2025-09-03 15:56:03 +0100

    yea

--- frontend/src/lib/schemas/login.schema.ts
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod';
-import {username, password} from './user.schema';
-export const loginSchema = z.object({
-	username,
-	password,
-});
-
-export type LoginSchema = z.infer<typeof loginSchema>;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,45 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-	createBrowserRouter,
-	Outlet,
-	RouterProvider,
-} from 'react-router-dom';
-import Root from './routes/root';
-import {Theme} from '@radix-ui/themes';
-import '@radix-ui/themes/styles.css';
-import './styles/fonts.css';
-import './styles/index.css';
-import './styles/theme-config.css';
-import ErrorPage from './error.page';
-import Layout from './routes/layout';
-import RegisterPage from './routes/register/register.page';
-import LoginPage from './routes/login/login.page';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Syscrack from "./Syscrack";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./global.css";
 
-const router = createBrowserRouter([
-	{
-		element: <Layout> <Outlet /> </Layout>,
-		path: '/',
-		errorElement: <ErrorPage/>,
-		children: [
-			{
-				index: true,
-				element: <Root />,
-			},
-			{
-				path: '/user/register',
-				element: <RegisterPage/>,
-			},
-			{
-				path: '/user/login',
-				element: <LoginPage/>,
-			},
-		],
-	},
-]);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Syscrack />
+  </React.StrictMode>
 );
